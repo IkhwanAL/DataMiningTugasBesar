39aface194bacbaad59b0bfb9fbab8629a46f3b8
Ikhwan A latif <ikhwanal235@gmail.com>
2020-12-31 11:06:59 +0700

    Update Regression.r

--- Regression.r
+++ Regression.r
@@ -1,7 +1,9 @@
-setwd("/Users/muhammadrasyidkhaikal/Desktop/Kuliah/Data\ Mining\ TB/DataMiningTugasBesar")
+#setwd("/Users/muhammadrasyidkhaikal/Desktop/Kuliah/Data\ Mining\ TB/DataMiningTugasBesar")
+setwd("D:/Semester 3/Data Mining/Tugas Besar/DataMiningTugasBesar")
 dataset <- read.csv("StudentsPerformance.csv" , sep = ",")
 head(dataset)
 
+# Korelasi Antara NIlai Math, Reading, write
 cor(dataset$math.score, dataset$reading.score)
 cor(dataset$math.score, dataset$writing.score)
 
@@ -11,6 +13,31 @@ cor(dataset$reading.score, dataset$writing.score)
 cor(dataset$writing.score, dataset$math.score)
 cor(dataset$writing.score, dataset$reading.score)
 
-linearMod <- lm(math.score ~ reading.score + writing.score, dataset)
+# Model Regression
+linearMath <- lm(math.score ~ reading.score + writing.score, dataset)
 summary(linearMod)
 
+linearRead <- lm(reading.score ~ math.score + writing.score, dataset)
+summary(linearRead)
+
+linearWrite <- lm(writing.score ~ reading.score + math.score, dataset)
+summary(linearWrite)
+
+# Soal Prediksi
+dataFindMath <- data.frame(
+  writing.score = 76,
+  reading.score = 69
+)
+dataFindRead <- data.frame(
+  math.score = 88,
+  writing.score = 84
+)
+dataFindWrite <- data.frame(
+  math.score = 88,
+  reading.score = 70
+)
+
+#Prediksi Nilai 
+predict(linearMath, dataFindMath)
+predict(linearRead, dataFindRead)
+predict(linearWrite, dataFindWrite)
